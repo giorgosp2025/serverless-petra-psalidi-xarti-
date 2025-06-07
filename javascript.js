d8d5b5abb7e27da224c9c7ff4085eeaf7d2a0f9a
giorgosp2025 <giorgospetpao@gmail.com>
2025-06-07 18:33:09 +0300

    teliki morfh vol.3

---
+++ javascript.js
@@ -0,0 +1,59 @@
+let userScr = 0;
+let drawScr = 0;
+let cpuScr = 0;
+
+function makeChoice(userChoice) {
+
+    document.getElementById("choices").style.display = "none"; // Απόκρυψη όταν έχει πατηθεί μία επιλογή
+
+    fetch('/.netlify/functions/game', {
+        method: 'POST',
+        body: JSON.stringify({ userChoice: userChoice }),
+        headers: {
+            'Content-Type': 'application/json'
+        }
+    })
+        .then(response => response.json())
+        .then(data => {
+
+            document.getElementById('user_choiceText').innerText = `Επέλεξες: ${userChoice}`;
+            document.getElementById('cpu_choiceText').innerText = `Επέλεξε: ${data.computerChoice}`;
+            document.getElementById('result').innerText = data.result;
+
+            //Ενημέωση των σκορ
+            if (data.result === "Κέρδισες!") {
+                userScr++;
+            }
+            else if (data.result === "Ισοπαλία!") {
+                drawScr++;
+            }
+            else {
+                cpuScr++;
+            }
+
+            // Ενημέρωση του scoreboard στο header
+            document.getElementById('user_score').innerText = userScr;
+            document.getElementById('draws_score').innerText = drawScr;
+            document.getElementById('cpu_score').innerText = cpuScr;
+
+            //κουμπί παίξε ξανά
+            document.getElementById("result-display").style.display = "block";
+            document.getElementById("play-again").style.display = "block";
+        });
+}
+
+function resetGame() {
+    // Επανεμφάνιση των επιλογών
+    document.getElementById("choices").style.display = "block";
+
+    // Απόκρυψη του αποτελέσματος και των επιλογών που εμφανίστηκαν μετά το γύρο
+    document.getElementById("result-display").style.display = "none";
+
+    // Απόκρυψη του κουμπιού "Παίξε Ξανά"
+    document.getElementById("play-again").style.display = "none";
+
+    // Καθαρισμός κειμένων
+    document.getElementById("user_choiceText").innerText = "";
+    document.getElementById("cpu_choiceText").innerText = "";
+    document.getElementById("result").innerText = "";
+}
